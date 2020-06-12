cb103514803bbc832d55237263a2b50376deb9e4
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-06-11 19:07:23 -0500

    Set up index with express and routers

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code away!
+const express = require("express")
+const cors = require("cors")
+const postRouter = require("./posts/postRouter")
+const userRouter = require("./users/userRouter")
+
+const server = express()
+const port = 5000
+
+server.use(express.json())
+server.use(cors())
+
+server.use("api/users", userRouter)
+server.use("api/users/:id/posts", postRouter)
+
+server.use((err, req, res, next) => {
+    console.log(err)
+    res.status(500).json({
+        message: "Error"
+    })
+})
+
+server.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`)
+})
